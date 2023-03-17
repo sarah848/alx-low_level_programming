28d46781567133b3105c8c5379cb241530624f84
sarah848 <bosiborisarah21@gmail.com>
2023-03-17 00:33:20 -0700

    Created a new directory(0x17-doubly_linked_lists) and added new files.

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -19,3 +19,17 @@ size_t print_listint(const listint_t *h){
 	}
 	return (nodes);
 }
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+	    int n;
+	        struct listint_s *next;
+} listint_t;
